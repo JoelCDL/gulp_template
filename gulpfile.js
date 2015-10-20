98c343fc05bfc2ca8cfa0384d74ac47410c53953
JoelUCOP <joel.hagedorn@ucop.edu>
2015-10-19 17:00:11 -0700

    add html validation task to build task

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,7 @@ gulp.task('default', function (callback) {
 // Run the build process by running "gulp build" at the command line:
 gulp.task('build', function (callback) {
   runSequence('clean', 
-    ['sass', 'useref', 'images', 'fonts'],
+    ['sass', 'useref', 'images', 'fonts', 'validateHTML'],
     callback
   )
 })
