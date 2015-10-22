d84c87bf71e10b9ee9fc105e4afd918847d2c8f4
JoelUCOP <joel.hagedorn@ucop.edu>
2015-10-22 11:12:24 -0700

    reorganize custom js examples

--- app/js/custom2.js
+++ app/js/custom2.js
@@ -1,7 +1,7 @@
-/* Main JavaScript */
+// ##### Custom JavaScript Example 2 ##### //
 
 $(document).ready(function(){
     $('.disappear').click(function(){
         $(this).hide();
     });
-}); 
+});
